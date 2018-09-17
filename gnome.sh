d7c1d2398a64f9c33d987b0ca6242542284fbdb6
vusi <baloyi.trev@gmail.com>
2018-09-17 11:52:24 +0200

    added script to initialize nodejs web app projects

---
+++ gnome.sh
@@ -0,0 +1,4 @@
+
+#move window buttons to the right
+gsettings set org.gnome.desktop.wm.preferences button-layout 'close,minimize,maximize:'
+
---
+++ install.sh
@@ -0,0 +1,4 @@
+#software to install on fresh installataion for arch
+
+sudo pacman -Syu
+sudo pacman -S vlc 
